187a622eb770cf372858238988e3d61319a066a0
Emmanuil <emmanuil.litvinenko@inbox.ru>
2023-11-25 00:48:01 +0500

    testing tag second

--- ABTC-i.py
+++ ABTC-i.py
@@ -1,4 +1,4 @@
-# test tag - v1.0.0
+# test tag - v1.0.1
 # verification - название объекта QPushButton - 'Верефикация'
 # openingButton - название объекта QPushButton - 'Открыть...'
 # saveButton - название объекта QPushButton - 'Сохранить'
